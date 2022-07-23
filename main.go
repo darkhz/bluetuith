f5b21df322cb4ae97d58b5f5331af7dc2704da51
darkhz <kmachanwenw@gmail.com>
2022-07-23 17:01:19 +0530

    main: Print error messages before application exit

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/darkhz/bluetuith/agent"
 	"github.com/darkhz/bluetuith/bluez"
 	"github.com/darkhz/bluetuith/cmd"
 	"github.com/darkhz/bluetuith/ui"
 )
 
+func errMessage(err string) {
+	fmt.Fprintf(os.Stderr, "\rError: %s\n", err)
+}
+
 func main() {
 	bluezConn, err := bluez.NewBluez()
 	if err != nil {
+		errMessage("Could not initialize bluez DBus connection")
 		return
 	}
 
 	cmd.ParseCmdFlags(bluezConn)
 
 	if err := agent.SetupAgent(bluezConn.Conn()); err != nil {
+		errMessage("Could not setup bluez agent")
 		return
 	}
 
